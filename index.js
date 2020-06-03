9e39ae2e9978cf4c9533e3ee13332ac0743c2038
ARPIT ANAND <44193921+arpitjson@users.noreply.github.com>
2020-06-03 17:06:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+
+});
+
+function Function() {
+  var linkInput = document.getElementById("link-input");
+  linkInput.select();
+  linkInput.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  // alert("Copied the text: " + linkInput.value);
+}
